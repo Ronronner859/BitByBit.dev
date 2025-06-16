af3452418546c4995c6a52800a67ad669025955e
Beak <chenyilong916002@gmail.com>
2025-06-17 00:50:53 +0800

    update logo

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'A speck in the byte-sea • Maker @ByteGarage',
+  author: '沧海一粟',
+  headerTitle: '沧海一粟@bytegarage',
+  description: 'Full-stack developer @ByteGarage, Building things with code. Crafting code, pixel by pixel. Code. Ship. Repeat.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
